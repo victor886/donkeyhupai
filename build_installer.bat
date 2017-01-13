f2f191f60a0a79d6161d739ce8170cf1b3d04f37
lipengcheng02 <lipengcheng02@baidu.com>
2017-01-13 17:18:16 +0800

    build to exe bat upload

---
+++ build_installer.bat
@@ -0,0 +1,15 @@
+rmdir /q /s %cd%\build
+rmdir /q /s %cd%\dist
+rmdir /q /s %cd%\沪牌助手-2017年1月版
+del %cd%\bid.spec
+del %cd%\沪牌助手-2017年1月版.rar
+C:\Python27\Scripts\pyinstaller --windowed --onefile --icon=%cd%\image\paimai.ico -F %cd%\bid.py
+xcopy %cd%\image %cd%\dist\image /e /i /y 
+xcopy %cd%\conf %cd%\dist\conf /e /i /y
+xcopy %cd%\tessdata %cd%\dist\tessdata /e /i /y
+copy %cd%\使用说明.txt %cd%\dist /y 
+mkdir %cd%\dist\log
+ren %cd%\dist\bid.exe 沪牌助手-2017年1月版.exe
+xcopy %cd%\dist %cd%\沪牌助手-2017年1月版 /e /i /y
+rar a -r 沪牌助手-2017年1月版.rar 沪牌助手-2017年1月版
+pause
